024f57136757521a83a11d2a5e6b918a8045c9b4
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-03 23:00:07 -0500

    create a function to return a promise with a data of api

---
+++ src/helpers/WeatherFetch.js
@@ -0,0 +1,18 @@
+export const WeatherFetch = async(ct) => {
+    const key = "5e01649e42a949268682b984b6012bd0";
+    const resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ct}&appid=${key}`);
+    const {main:temp,name,sys,weather} = await resp.json();
+    const climate={
+        temps: ConCentigrade(temp.temp),
+        name:name,
+        contry:sys.country,
+        weather:weather
+    }
+
+    return climate;
+};
+
+
+const ConCentigrade = (data)=>{
+    return data-273.15
+}
